8ff29f7c846c546258928111aa4a87a651483823
Vladyslav Levitski <vladyslav.levitski@tieto.com>
2020-01-10 04:26:26 +0200

    Add solution for Ex1 and Ex2 lecture9
    Create tasks and delete
    Assign task priority change priority

---
+++ lecture9/task_1_2/MDK-ARM/RTE/_task_1_2/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'task_1_2' 
+ * Target:  'task_1_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
